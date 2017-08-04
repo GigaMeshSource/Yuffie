87bf789fad00213511e2908cc1def613a01a61a9
HolyNoodle <kbriffaut@gmail.com>
2017-08-04 15:41:34 +0200

    Clean and fix debug

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -13,20 +13,11 @@ namespace WebApp.Controllers
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Admin()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
+        
         public IActionResult Error()
         {
             return View();
