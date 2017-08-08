dabf0a3561739851f91df0c6b19207d3fad0a676
HolyNoodle <kbriffaut@gmail.com>
2017-08-08 18:50:42 +0200

    working on validation and style

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -1,9 +1,75 @@
-﻿$(function() {
+﻿var rules = [ {
+    "Key": "Ex conjoint vivant",
+    "StartingEffect": {
+        "Css": {
+            "display": "none"
+        }
+    },
+    "Conditions": [{
+        "Key": "Status marital",
+        "Value": "Item3",
+    }],
+    "Effect": {
+        "Css": {
+            "display": "block"
+        }
+    }
+}]
+
+$(function() {
     var elements = []
     var getElement = function(key){
+        key = key.replace(/ /ig, "_")
         for(var i = 0; i < elements.length; ++i) {
             if(elements[i].Key == key) {
                 return elements[i]
+            }
+        }
+        return null
+    }
+
+    var getRule = function(key){
+        for(var i = 0; i < rules.length; ++i) {
+            if(rules[i].Key == key) {
+                return rules[i]
+            }
+        }
+        return null
+    }
+
+    var processEffect = function(key, effect) {
+        if(effect.Css != undefined && effect.Css != null) {
+            var element = getElement(key)
+            var block = $($("#" + element.Key).parents("[element]"))
+            for(var property in effect.Css) {
+                block.css(property, effect.Css[property])
+            }
+        }
+    }
+
+    var initEffect = function() {
+        for(var i = 0; i < rules.length; ++i) {
+            var rule = rules[i]
+            if(rule.StartingEffect != undefined && rule.StartingEffect != null) {
+                processEffect(rule.Key, rule.StartingEffect)
+            }
+        }
+    }
+
+    var processChange = function() {
+         for(var i = 0; i < rules.length; ++i) {
+            var rule = rules[i]
+            var result = true
+            for(var i = 0; i < rule.Conditions.length; ++i) {
+                var condition = rule.Conditions[i]
+                var element = getElement(condition.Key)
+                if(element == null || element.Value != condition.Value) {
+                    result = false
+                    break
+                }
+            }
+            if(result) {
+                processEffect(rule.Key, rule.Effect)
             }
         }
     }
@@ -22,13 +88,15 @@
                 if(e.keyCode == 13) { //enter
                     watchElement.append("<li>" + inp.val() + "</li>")
                     element.Value.push(inp.val())
+                    processChange()
                     inp.val("")
                 }
             })  
         }
         if(watchElement.is("input") || watchElement.is("select")) {
             watchElement.change(function() {
-                element.Value = watchElement.val()                
+                element.Value = watchElement.val()
+                processChange()
             })
         }
 
@@ -36,18 +104,22 @@
             watchElement.change(function() {
                 if(watchElement.attr("checked")) {
                     element.Value = watchElement.val()
+                    processChange()
                 }
             })
         }
 
         if(watchElement.is("[type=checkbox]")) {
             watchElement.change(function() {
-                element.Value = watchElement.prop("checked")                
+                element.Value = watchElement.prop("checked")
+                processChange()
             })
         }
 
         elements.push(element);
     })
+
+    initEffect()
     
     $("#yuffieValidate").click(function(){
         console.log(elements)
@@ -60,4 +132,6 @@
             }
         });
     })
+
+ 
 })
