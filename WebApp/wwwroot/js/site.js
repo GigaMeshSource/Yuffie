aae1b425084d776dd489382c793f5a4a4a5d825f
Drevon Thibault <thibault.drevon@gmail.com>
2017-08-31 15:05:30 +0200

    MEEEEEEEEEEEEEEEEEEEEEEEEEEEEE———RGERD = MERGED

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -384,7 +384,7 @@ $(function() {
 
                 e.Value.push(obj)
                 
-                $("#con_" + e.Key + " [watch-list]").append("<li><div class='chip'>" + id + "</div></li>")
+                $("#con_" + e.Key + " [watch-list]").append("<div class='chip'>" + id + "<i class='close material-icons'>close</i></div>")
 
                 processChange()
                 copySummary(e)
@@ -451,15 +451,40 @@ $(function() {
 
 
     $('select').material_select();
+    
     $('.collapsible').collapsible();
+
+    $('.chips-placeholder').material_chip({
+        placeholder: 'Participants ajoutés',
+        secondaryPlaceholder: '+ Participants',
+      });
+
     $('.datepicker').pickadate({
-    selectMonths: false, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year,
-    today: 'Today',
-    clear: 'Clear',
-    close: 'Ok',
-    closeOnSelect: true // Close upon selecting a date,
+        selectMonths: false, // Creates a dropdown to control month
+        selectYears: 15, // Creates a dropdown of 15 years to control year,
+        today: 'Today',
+        clear: 'Clear',
+        close: 'Ok',
+        closeOnSelect: true, // Close upon selecting a date,
+        monthsFull: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+        monthShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+        weekdaysShort: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+        weekdaysLetter: [ 'D', 'L', 'M', 'M', 'J', 'V', 'S' ],
+        today: 'aujourd\'hui',
+        clear: 'effacer',
     });
+
+    $('.timepicker').pickatime({
+        default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+        fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+        twelvehour: false, // Use AM/PM or 24-hour format
+        donetext: 'OK', // text for done-button
+        cleartext: 'Clear', // text for clear-button
+        canceltext: 'Cancel', // Text for cancel-button
+        autoclose: false, // automatic close timepicker
+        ampmclickable: true, // make AM PM clickable
+        aftershow: function(){} //Function for after opening timepicker
+      });
 
     $(".yuffieNextButton").click(function(e) {
         var parent = $($(e.target).parents("li"));
