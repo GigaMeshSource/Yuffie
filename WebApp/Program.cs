166805c83390af1657b4dc82848880045d158db9
Anne-Laure <annelaure.trabon@gmail.com>
2017-10-26 16:34:13 +0200

    remove ports

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -14,7 +14,6 @@ namespace WebApp
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseUrls("http://0.0.0.0:5000")
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
